8c0103a20109630ddf6a59f4ec4f4a359f8fce9e
Jaironec <16cardenas16@gmail.com>
2021-07-31 23:48:32 -0500

    Deber Funciones Vectores

---
+++ cmake-build-debug/CMakeFiles/Vectores_funciones.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vectores_funciones.dir/main.cpp.obj"
+  "Vectores_funciones.exe"
+  "Vectores_funciones.exe.manifest"
+  "Vectores_funciones.pdb"
+  "libVectores_funciones.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vectores_funciones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
